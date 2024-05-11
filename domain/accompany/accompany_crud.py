f81d87e6ebf73d6e38b7ba5bde37e68f2f1093e1
mingyu <jimbo980858@gmail.com>
2024-05-11 16:20:45 +0900

    Fix: 동행 가입 방식 변경 가능하게 수정 ver2

--- domain/accompany/accompany_crud.py
+++ domain/accompany/accompany_crud.py
@@ -195,7 +195,10 @@ def update_accompany(db: Session, db_accompany: Accompany, accompany_update: Acc
     db_accompany.activity_scope = accompany_update.activity_scope
     db_accompany.introduce = accompany_update.introduce
     db_accompany.total_member = accompany_update.total_member
-    db_accompany.qna = accompany_update.qna
+    if accompany_update.qna is None:
+        db_accompany.qna = None
+    else:
+        db_accompany.qna = accompany_update.qna
     db_accompany.update_date = datetime.now()
 
     current_images = get_image_by_accompany_id(db, accompany_id=db_accompany.id)
