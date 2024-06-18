eb5ca208cf0735cbb4fe3bf6a65decc245aa1a83
rbalsJ <jimbo980858@gmail.com>
2024-06-19 02:26:00 +0900

    github action test

--- bluegreen/deploy_blue_green.sh
+++ bluegreen/deploy_blue_green.sh
@@ -1,66 +1,64 @@
 #!/bin/bash
-set -e # 스크립트가 오류 발생 시 종료되도록 설정
-
-echo "Checking current running container..."
 IS_GREEN=$(docker ps | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
 
-if [ -z "$IS_GREEN" ]; then # blue라면
+if [ -z $IS_GREEN  ];then # blue라면
+
   echo "### BLUE => GREEN ###"
 
   echo "1. get green image"
-  docker-compose pull green || { echo "Failed to pull green image"; docker compose logs green; exit 1; }
+  docker-compose pull green # green으로 이미지를 내려받습니다.
 
   echo "2. green container up"
-  docker-compose up -d green || { echo "Failed to start green container"; docker compose logs green; exit 1; }
+  docker-compose up -d green # green 컨테이너 실행
 
-  while true; do
+  while [ 1 = 1 ]; do
     echo "3. green health check..."
     sleep 3
 
-    REQUEST=$(curl -s http://127.0.0.1:8080) # green으로 request
-    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-      echo "health check success"
-      break
-    fi
-  done
+    REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
+      if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+        echo "health check success"
+        break ;
+      fi
+  done;
 
   echo "4. reload nginx"
   sudo cp -p /etc/nginx/nginx-green.conf /etc/nginx/nginx.conf || exit 1
-  sudo systemctl restart nginx || exit 1
+  sudo sudo systemctl restart nginx || exit 1
 
   if [ "$(docker ps -q -f name=blue)" ]; then
     echo "5. blue container down"
-    docker-compose stop blue || { echo "Failed to stop blue container"; docker compose logs blue; exit 1; }
+    docker-compose stop blue
   fi
 
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker-compose pull blue || { echo "Failed to pull blue image"; docker compose logs blue; exit 1; }
+  docker-compose pull blue
 
   echo "2. blue container up"
-  docker-compose up -d blue || { echo "Failed to start blue container"; docker compose logs blue; exit 1; }
+  docker-compose up -d blue
 
-  while true; do
+  while [ 1 = 1 ]; do
     echo "3. blue health check..."
     sleep 3
-    REQUEST=$(curl -s http://127.0.0.1:8081) # blue로 request
+    REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
 
     if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
       echo "health check success"
-      break
+      break ;
     fi
 
-  done
+  done;
 
   echo "4. reload nginx"
   sudo cp -p /etc/nginx/nginx-blue.conf /etc/nginx/nginx.conf || exit 1
-  sudo systemctl restart nginx || exit 1
+  sudo sudo systemctl restart nginx || exit 1
 
   if [ "$(docker ps -q -f name=green)" ]; then
     echo "5. green container down"
-    docker-compose stop green || { echo "Failed to stop green container"; docker compose logs green; exit 1; }
+    docker-compose stop green
   fi
 
 fi
